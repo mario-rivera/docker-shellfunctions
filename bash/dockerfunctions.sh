4e2d497132ddbc172b8f4ac3b9bb95f31112580b
ESPC\BSwierczynski <bozydar.swierczynski@espc.com>
2017-06-21 12:23:43 +0100

    Add some messages for errors in bash/dockerfunctions; add .idea to .gitignore

--- bash/dockerfunctions.sh
+++ bash/dockerfunctions.sh
@@ -4,25 +4,25 @@ DOCKER_BASH_FUNCTIONS_DIR=$(dirname "${BASH_SOURCE[0]}")
 create_network_ifnotexists(){
     # -z is if output is empty
     if [ -z "$(docker network ls | grep $1)" ]; then
-        echo "Creating network $1"
         docker network create $1
+        echo "Network $1 created"
     fi
 }
 
 remove_network(){
-    # if network exists remove
     if [ ! -z "$(docker network ls | grep $1)" ]; then
-        echo "Removing network $1"
         docker network rm $1
+        echo "Network $1 removed"
+    else
+        echo "Tried to remove network $1, but it does not exist"
     fi
 }
 
 connect_to_network(){
-    # -z if output is empty
     if [ -z "$(docker network inspect $1 -f '{{range $value := .Containers}}{{ $value }}{{end}}' | grep -w "$2")" ]; then
         echo "Container $2 is not connected to network $1"
-        echo "Connecting container $2 to network $1"
         docker network connect $1 $2
+        echo "Connected container $2 to network $1"
     fi
 }
 
@@ -35,7 +35,7 @@ disconnect_from_network(){
 
 check_container_ps(){
     local result=$(eval $1)
-    
+
     if [ ! -z "$result" ]; then
         # 0 = true
         return 0
@@ -46,11 +46,11 @@ check_container_ps(){
 }
 
 container_is_stopped(){
-    check_container_ps "docker ps -aq -f status=exited -f name=$1 | grep -w \"\\s$1\$\""
+    check_container_ps "docker ps -a -f status=exited -f name=$1 | grep -w \"\\s$1\$\""
 }
 
 container_is_running(){
-    check_container_ps "docker ps -aq -f status=running -f name=$1 | grep -w \"\\s$1\$\""
+    check_container_ps "docker ps -a -f status=running -f name=$1 | grep -w \"\\s$1\$\""
 }
 
 container_exists(){
@@ -60,13 +60,23 @@ container_exists(){
 stop_container(){
     if container_is_running $1; then
         docker stop $1
+        echo "Container $1 stopped"
+    else
+        echo "Tried to stop $1, but it is not running"
     fi
 }
 
 remove_container(){
     stop_container $1
-    
+
     if container_exists $1 && container_is_stopped $1; then
         docker rm $1
+        echo "Container $1 removed"
+    else
+        if ! container_exists $1; then
+            echo "Tried to remove $1 container but it does not exist"
+        elif ! container_is_stopped $1; then
+            echo "Tried to remove $1 container but it is not stopped"
+        fi
     fi
 }
